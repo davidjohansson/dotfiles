9e3f9616d6442398b5fafcb18ffd3d87d074d339
David Johansson <david.johansson4@tre.se>
2022-05-15 09:01:32 +0200

    Initial

---
+++ zsh/aliases.zsh
@@ -0,0 +1,118 @@
+# Davids own stuff
+alias git='LANG=en_US git'
+alias cl="clear"
+alias msr="mvn spring-boot:run"
+alias xopen="xargs -I {} open {}"
+alias xcurl="xargs -I {} curl -X 'GET' {} -H 'accept: application/json'"
+alias xnmd="xargs nvim '+set nospell'"
+alias typora="open -a typora"
+alias s="scmd"
+
+alias xtyp="xargs open -a typora"
+alias zx="zx --quite"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+alias pd="pushd ~/.dotfiles"
+
+alias k="kubectl --kubeconfig ~/.kube/config-bt1 -n sales"
+alias kbt1="kubectl --kubeconfig ~/.kube/config-bt1 -n sales"
+alias knr="kubectl --kubeconfig ~/.kube/config-nr -n sales"
+alias kpr="kubectl --kubeconfig ~/.kube/config-prd -n sales"
+alias ksu="kubectl --kubeconfig ~/.kube/config-support -n sales"
+alias kos="kubectl --kubeconfig ~/.kube/config-os -n sales-poc"
+alias ocos="oc --config=~/.kube/config-os -n sales-poc"
+
+alias k9sbt1="k9s --kubeconfig ~/.kube/config-bt1 -n sales"
+alias k9snr="k9s --kubeconfig ~/.kube/config-nr -n sales"
+alias k9spr="k9s --kubeconfig ~/.kube/config-prd -n sales"
+alias k9ssu="k9s --kubeconfig ~/.kube/config-support -n delivery"
+alias k9sos="k9s --kubeconfig ~/.kube/config-os -n sales-poc"
+
+alias eg="exa --long --header --inode --git"
+alias b="buku --suggest"
+alias bm="buku -p -f 40 | fzf | cut -f1"
+alias bmo="buku -p -f 40 | fzf | cut -f1 | xopen"
+alias bmp="buku -p -f 40 | fzf | cut -f1 | tee >(pbcopy)"
+alias bms="buku -p -f 40 | fzf | cut -f1  | swagger | xopen"
+
+alias disp1="/usr/local/bin/display_manager.py res 2560 1490 60"
+
+alias disp2="/usr/local/bin/display_manager.py res 3360 1890 60"
+
+# reload zsh config
+alias reload!='RELOAD=1 source ~/.zshrc'
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+    colorflag="--color"
+else # macOS `ls`
+    colorflag="-G"
+fi
+
+# use nvim, but don't make me think about it
+alias vim="nvim"
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
+
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# Trim new lines and copy to clipboard
+alias trimcopy="tr -d '\n' | pbcopy"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+# remove broken symlinks
+alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
+
+# File size
+alias fs="stat -f \"%z bytes\""
+
+# ROT13-encode text. Works for decoding, too! ;)
+alias rot13='tr a-zA-Z n-za-mN-ZA-M'
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# One of @janmoesen’s ProTip™s
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+    alias "$method"="lwp-request -m '$method'"
+done
+
+# Kill all the tabs in Chrome to free up memory
+# [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
+alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+alias chrome="/Applications/Google\\ \\Chrome.app/Contents/MacOS/Google\\ \\Chrome"
+alias canary="/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\ Chrome\\ Canary"
+
+alias pcat='pygmentize -f terminal256 -O style=native -g'
---
+++ zsh/functions.zsh
@@ -0,0 +1,107 @@
+####################
+# functions
+####################
+
+function swagger() {
+    awk '{print $1"/swagger-ui/index.html"}'
+}
+
+function zshaddhistory() {
+	echo "${1%%$'\n'}|${PWD}   " >> ~/.zsh_history_ext
+}
+
+function md() {
+    mkdir -p $1
+    cd $1
+}
+
+function ng-stop() {
+    sudo launchctl stop homebrew.mxcl.nginx
+}
+
+function ng-start() {
+    sudo launchctl start homebrew.mxcl.nginx
+}
+function ng-restart() {
+     sudo launchctl start homebrew.mxcl.nginx
+}
+
+function dns-restart() {
+    sudo launchctl stop homebrew.mxcl.dnsmasq
+    sudo launchctl start homebrew.mxcl.dnsmasq
+}
+
+pretty() {
+    pygmentize -f terminal256 $* | less -R
+}
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+    local port="${1:-8000}"
+    open "http://localhost:${port}/"
+    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+    python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+}
+
+# get gzipped size
+function gz() {
+    echo "orig size    (bytes): "
+    cat "$1" | wc -c
+    echo "gzipped size (bytes): "
+    gzip -c "$1" | wc -c
+}
+
+# All the dig info
+function digga() {
+    dig +nocmd "$1" any +multiline +noall +answer
+}
+
+# Escape UTF-8 characters into their 3-byte format
+function escape() {
+    printf "\\\x%s" $(printf "$@" | xxd -p -c1 -u)
+    echo # newline
+}
+
+# Decode \x{ABCD}-style Unicode escape sequences
+function unidecode() {
+    perl -e "binmode(STDOUT, ':utf8'); print \"$@\""
+    echo # newline
+}
+
+fag(){
+  local line
+  line=`ag --nocolor "$1" | fzf` \
+    && vim $(cut -d':' -f1 <<< "$line") +$(cut -d':' -f2 <<< "$line")
+}
+
+# alternative using ripgrep-all (rga) combined with fzf-tmux preview
+# implementation below makes use of "open" on macOS, which can be replaced by other commands if needed.
+# allows to search in PDFs, E-Books, Office documents, zip, tar.gz, etc. (see https://github.com/phiresky/ripgrep-all)
+# find-in-file - usage: fcif <searchTerm> or fif "string with spaces" or fif "regex"
+
+fif() {
+    if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+    local file
+    file="$(rga --max-count=1 --ignore-case --files-with-matches --no-messages "$@" | fzf-tmux -r 100%  --preview='bat --style=numbers --color=always --line-range :500 {}')" && idea "$file"
+}
+
+
+frf() {
+    if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+    local file
+    file="$(rga --max-count=1 --ignore-case --files-with-matches --no-messages "$@" | fzf-tmux -r 100%  --preview="rga --ignore-case --pretty --context 30 '"$@"' {}")" && nvim "$file"
+}
+
+
+fof() {
+if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+}
+
+fef() {
+if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+file="$(rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}")" && nvim "$file"
+}
+
+
